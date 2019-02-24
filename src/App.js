a3c32667e191bf694e8597ddd048f5ae4809b2d6
kamerk22 <kashyapk62@gmail.com>
2019-02-24 21:32:52 +0530

    change UI and Slider design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      length: 8
+    };
+  }
   render() {
     return (
       <div className="container">
         <section>
           <header>
-            <h1>Random Password Generator</h1>
+            <p style={{ marginBottom: "-10px" }}>
+              Random Password Generator Tool
+            </p>
+            <h1>Generate a secure password</h1>
           </header>
           <input id="password" name="password" type="text" readOnly />
         </section>
@@ -16,36 +25,69 @@ class App extends Component {
             <h3>Customize your password</h3>
           </header>
           <fieldset>
-            <div class="form-group">
-              <label for="email">Password Length:</label>&nbsp;
-              <input type="number" style={{ width: 50 }} /> &nbsp;
-              <input className="progress-bar" type="range" min="1" max="100" />
-            </div>
-            <div class="form-group">
-              <input for="check" type="checkbox" id="check" /> &nbsp;
-              <label for="check">Uppercase:</label>
-              <br />
-              <input for="check" type="checkbox" id="check" /> &nbsp;
-              <label for="check">Lowercase:</label>
-              <br />
-              <input for="check" type="checkbox" id="check" /> &nbsp;
-              <label for="check">Numbers:</label>
-              <br />
-              <input for="check" type="checkbox" id="check" /> &nbsp;
-              <label for="check">Symbols:</label>
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <input htmlFor="check" type="checkbox" id="check" /> &nbsp;
+                  <label htmlFor="check">Uppercase:</label>
+                  <br />
+                  <input htmlFor="check" type="checkbox" id="check" /> &nbsp;
+                  <label htmlFor="check">Lowercase:</label>
+                  <br />
+                  <input htmlFor="check" type="checkbox" id="check" /> &nbsp;
+                  <label htmlFor="check">Numbers:</label>
+                  <br />
+                  <input htmlFor="check" type="checkbox" id="check" /> &nbsp;
+                  <label htmlFor="check">Symbols:</label>
+                </div>
+              </div>
+              <div className="col">
+                <div className="form-group">
+                  <label htmlFor="email">Password Length:</label>&nbsp;
+                  <input
+                    type="number"
+                    min="8"
+                    max="40"
+                    style={{ width: 65 }}
+                    value={this.state.length}
+                    onChange={e => {
+                      console.log(e.target.value);
+                      this.setState({ length: e.target.value });
+                    }}
+                  />{" "}
+                  &nbsp;
+                  <div class="slider-container">
+                    <input
+                      className="slider"
+                      type="range"
+                      min="8"
+                      max="40"
+                      value={this.state.length}
+                      onChange={e => {
+                        console.log(e.target.value);
+                        this.setState({ length: e.target.value });
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </fieldset>
-          <br/>
+          <br />
           <div style={{ textAlign: "center" }}>
-            <button class="btn  btn-primary">Generate</button> &nbsp;
-            &nbsp;
-            <button class="btn btn-error">Copy Passoword</button>
+            <button className="btn  btn-primary">Generate</button> &nbsp; &nbsp;
+            <button className="btn btn-error">Copy Passoword</button>
             <br />
             <br />
-          </div>  
+          </div>
         </section>
-        <div style={{textAlign: 'center', position: 'absolute', bottom: 10, left: 0, right: 0, marginLeft :'auto', marginRight: 'auto'}}>
-        Made with <span style={{color: '#e25555'}}>&#9829;</span> by <a href="http://kamerk22.github.io">Kashyap Merai</a>
+        <div
+          style={{
+            textAlign: "center"
+          }}
+        >
+          Made with <span style={{ color: "#e25555" }}>&#9829;</span> by{" "}
+          <a href="http://kamerk22.github.io">Kashyap Merai</a>
         </div>
       </div>
     );
